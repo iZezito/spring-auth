bbd97031962d82cb2ed99b94c65200eaa785c2da
iZezito <emersonsilva81240@gmail.com>
2025-01-19 22:42:37 -0300

    fix(security): Corrige retorno incorreto de 401 para erros de aplicação

--- src/main/java/org/example/springauth/controllers/GlobalExceptionHandler.java
+++ src/main/java/org/example/springauth/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.springauth.controllers;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +14,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleNotFound(NoHandlerFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body("{\"message\": \"Recurso não encontrado\"}");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("{\"message\": \"Erro de integridade de dados - " + ex.getLocalizedMessage() + "\"}");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("{\"message\": \"Erro interno do servidor - " + ex.getMessage() + "\"}");
     }
 }
